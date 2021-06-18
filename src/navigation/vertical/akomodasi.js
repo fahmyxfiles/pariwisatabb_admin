7161d4c457a62a4e40efdaa690e5d43a2e1f7a81
urdopebae <77972350+urdopebae@users.noreply.github.com>
2021-06-18 16:02:05 +0800

    new view

---
+++ src/navigation/vertical/akomodasi.js
@@ -0,0 +1,43 @@
+export default [{
+        header: 'Akomodasi',
+    },
+    {
+        title: 'Hotel',
+        icon: 'WindIcon',
+        children: [{
+                title: 'Daftar Hotel',
+                route: 'apps-invoice-list',
+            },
+            {
+                title: 'Fasilitas Hotel',
+                route: 'apps-invoice-list',
+            },
+        ],
+    },
+    {
+        title: 'Wisma',
+        icon: 'FileTextIcon',
+        children: [{
+                title: 'Daftar Wisma',
+                route: 'apps-invoice-list',
+            },
+            {
+                title: 'Fasilitas Wisma',
+                route: 'apps-invoice-list',
+            },
+        ],
+    },
+    {
+        title: 'Homestay',
+        icon: 'FileTextIcon',
+        children: [{
+                title: 'Daftar Homestay',
+                route: 'apps-invoice-list',
+            },
+            {
+                title: 'Fasilitas Homestay',
+                route: 'apps-invoice-list',
+            },
+        ],
+    },
+]
--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -32,10 +32,11 @@ tagVariant
 */
 import dashboard from './dashboard'
 import appsAndPages from './apps-and-pages'
+import akomodasi from './akomodasi'
 import others from './others'
 import chartsAndMaps from './charts-and-maps'
 import uiElements from './ui-elements'
 import formAndTable from './forms-and-table'
 
 // Array of sections
-export default [...dashboard, ...appsAndPages, ...uiElements, ...formAndTable, ...chartsAndMaps, ...others]
+export default [...dashboard, ...akomodasi, ...appsAndPages, ...uiElements, ...formAndTable, ...chartsAndMaps, ...others]

54649ba5d48e645e430972fac578c58cb8f7be48
fahmyxfiles <fahmy.xfiles@gmail.com>
2021-07-03 03:19:58 +0800

    Update accomodation.js

--- src/router/routes/accomodation.js
+++ src/router/routes/accomodation.js
@@ -48,7 +48,7 @@ export default [{
                     },
                     {
                         text: 'Facilities',
-                        active: true,
+                        active: true
                     },
                     
                 ],
